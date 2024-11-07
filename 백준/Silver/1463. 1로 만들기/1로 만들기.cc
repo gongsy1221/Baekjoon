8a04bbf0147bf7d3cf670af995557b1c4a03fd1a
gongsy1221 <123607917+gongsy1221@users.noreply.github.com>
2024-11-07 10:40:14 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 41084 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 int dp[10000000];
 int main()
@@ -10,14 +9,14 @@ int main()
 
 	int n;
 	cin >> n;
-	
+
 	for (int i = 2; i <= n; i++)
 	{
 		dp[i] = dp[i - 1] + 1;
-		if (i % 2 == 0 && dp[i] > (dp[i / 2] + 1))
-			dp[i] = (dp[i / 2] + 1);
-		if (i % 3 == 0 && dp[i] > (dp[i / 3] + 1))
-			dp[i] = (dp[i / 3] + 1);
+		if (i % 2 == 0)
+			dp[i] = min(dp[i], (dp[i / 2] + 1));
+		if (i % 3 == 0)
+			dp[i] = min(dp[i], (dp[i / 3] + 1));
 	}
 
 	cout << dp[n];

ad941d1c63288b277fd9cbcfe610d70421feadcc
gongsy1221 <123607917+gongsy1221@users.noreply.github.com>
2024-12-09 17:25:36 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int dp[1000001] = { 1, 3};
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = (dp[i - 1] * 2 + dp[i - 2]) % 9901;
+    }
+
+    cout << dp[n];
+}

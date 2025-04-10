531a02d78dc7d44ca7a737d0f638fd23344828b3
gongsy1221 <123607917+gongsy1221@users.noreply.github.com>
2025-04-10 15:01:54 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << 2000 << '\n';
+	for (int i = 1; i <= 1000; i++)
+	{
+		cout << 1 << ' ';
+	}
+	for (int i = 1000; i >= 1; i--)
+	{
+		cout << 1000 << ' ';
+	}
+}

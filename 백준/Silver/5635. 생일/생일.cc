682504ecc028ff62c50867f36bc5c142aaa7d684
공서연 <123607917+gongsy1221@users.noreply.github.com>
2025-05-16 19:07:56 +0900

    [Silver V] Title: 생일, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/5635. 생일/생일.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+struct student 
+{
+	string name;
+	int day, mon, year;
+};
+
+bool cmp(const struct student& x, const struct  student& y) 
+{
+	if (x.year < y.year)
+		return true;
+
+	else if (x.year == y.year) 
+	{
+		if (x.mon < y.mon)
+			return true;
+		else if (x.mon == y.mon) 
+		{
+			if (x.day < y.day)
+				return true;
+			else
+				return false;
+		}
+		else
+			return false;
+	}
+	else
+		return false;
+}
+
+int main() 
+{
+	int n; 
+	cin >> n;
+	vector<student>arr;
+
+	for (int i = 0; i < n; i++) 
+	{
+		string x;
+		int a, b, c;
+		cin >> x >> a >> b >> c;
+		arr.push_back({ x,a,b,c });
+	}
+
+	sort(arr.begin(), arr.end(), cmp);
+
+	cout << arr[n - 1].name << '\n' << arr[0].name;
+}

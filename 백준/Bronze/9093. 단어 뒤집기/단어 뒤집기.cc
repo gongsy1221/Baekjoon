bc6558c551728c83bd4aa3fbdcfe6b6d4b3da40e
공서연 <123607917+gongsy1221@users.noreply.github.com>
2025-08-15 21:09:37 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 268 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+int main()
+{
+    int t;
+    string s;
+    stack <char> st;
+    cin >> t;
+    cin.ignore();
+
+    while (t--)
+    {
+        getline(cin, s);
+        s += ' ';
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == ' ')
+            {
+                while (!st.empty())
+                {
+                    cout << st.top();
+                    st.pop();
+                }cout << ' ';
+            }
+            else
+                st.push(s[i]);
+        }
+    }
+}
